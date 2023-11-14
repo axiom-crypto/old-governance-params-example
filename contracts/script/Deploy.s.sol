bd5437c4b442a4a2ab5fca924fbf2053fcb23719
Yu Jiang Tham <yoojbruin@gmail.com>
2023-11-14 13:30:24 +0300

    Update query schema

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -7,7 +7,7 @@ import { UselessNFT } from '../src/UselessNFT.sol';
 
 contract DeployScript is Script {
     address public constant AXIOM_V2_QUERY_GOERLI_MOCK_ADDR = 0xf15cc7B983749686Cd1eCca656C3D3E46407DC1f;
-    bytes32 public constant QUERY_SCHEMA = 0xe93618b9115b1605c24a348a97c1fa111cf06ec8c2f5b11f80b3784483898372;
+    bytes32 public constant QUERY_SCHEMA = 0x7c88a2fbb4e325cc98312cfb696f1345bf674c69cb9da43a9e0ac24e70b28d41;
 
     function setUp() public {}
 
